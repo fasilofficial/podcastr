33597a3e29b77a0fc131a42ef64ff38567c21cd2
fasilofficial <muhammedfasilofficial@gmail.com>
2024-07-11 19:29:36 +0530

    Add basic layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,11 +8,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
         <LeftSidebar />
-        {children}
-        <p className="text-white-1">RIGHT SIDEBAR</p>
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14 ">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div>
+              <Image />
+              MobileNav
+            </div>
+            <div>
+              Toaster
+              {children}
+            </div>
+          </div>
+        </section>
+        <RightSidebar />
       </main>
     </div>
   );
